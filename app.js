a3adccefc0d5c523919e7469acfc551de541b9de
MajorMaad <quentin.audinot@gmail.com>
2016-02-01 17:27:40 -0500

    Database connection (mongoose)

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var db = require('./config/db');
 
 var app = express();
+
+mongoose.connect(db.url);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  secret: 'secret',
+  url: ' mongodb://global:global@ds055505.mongolab.com:55505/express-example'
+};
